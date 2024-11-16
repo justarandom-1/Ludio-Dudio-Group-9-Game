daa75c4e4452985ddaf1d6ac4b9901182a2e6e26
just a random! <mchen8969@gmail.com>
2024-11-16 14:44:10 -0800

    Streamlined Level and Underwater Camera

--- Assets/MainCamera.cs
+++ Assets/MainCamera.cs
@@ -6,16 +6,23 @@ public class MainCamera : MonoBehaviour
 {
     private GameObject Player;
     private Transform PlayerTransform;
+
+    private Object Bubbles;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.Find("Player");
         PlayerTransform = Player.GetComponent<Transform>();
+        Bubbles = Resources.Load<GameObject>("Water/Bubbles");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(PlayerTransform.position.x + 3.5F, PlayerTransform.position.y, transform.position.z);
+        
+        if(Random.Range(0, 50) == 0){
+            Instantiate(Bubbles, new Vector3(Random.Range(-8F, 8F) + transform.position.x, -7.6F + transform.position.y, -7.5F), Quaternion.identity);
+        }
     }
 }
