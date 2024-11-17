556bec418407e3560e3950d27c3babb4bc9bd4d1
just a random! <mchen8969@gmail.com>
2024-11-16 18:58:44 -0800

    Created half of tilemap

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,15 +7,28 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D RB;
+    private Animator PlayerAnimator;
     private Vector2 MovementVector;
     [SerializeField] float speed;
+    private bool IsDashing;
     private void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        PlayerAnimator = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue value){
         MovementVector = value.Get<Vector2>();
+    }
+
+    void OnDash(InputValue value){
+        if(value.Get<float>() == 1){
+            IsDashing = true;            
+        }
+
+        if(value.Get<float>() == 0){
+            IsDashing = false;;
+        }
     }
 
     // Update is called once per frame
@@ -25,5 +38,10 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
         RB.velocity = MovementVector * speed;
+        PlayerAnimator.SetInteger("VerticalDirection", (int)MovementVector.y);
+
+        if(IsDashing){
+            RB.velocity = new Vector2(RB.velocity.x * 2, RB.velocity.y);
+        }
     }
 }
