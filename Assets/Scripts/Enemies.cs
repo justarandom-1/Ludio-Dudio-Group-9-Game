28ff4a42b33c86b692747ae5495ec3407a471b74
Iris Tsui <157786209+irist0419@users.noreply.github.com>
2024-11-17 01:07:04 -0800

    Animated Frog

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public abstract class Enemies : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    
+    // Update is called once per frame
+    public virtual void Update()
+    {
+    }
+
+
+}
